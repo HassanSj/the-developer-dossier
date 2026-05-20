12e6f03e0ce594736773069c73f65c832f2e1c7d
Hassan Sajjad <94786597+HassanSj@users.noreply.github.com>
2026-05-21 01:58:56 +0500

    Fix/NodeJS-Changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Hassan-Sajjad-Dev/',   // 🔥 THIS IS REQUIRED
   server: {
     port: 3000,
     proxy: {
